d98390c03a535e71ca6e74c3796d007e90236c51
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2020-02-17 08:47:38 +0000

    Finished Section 5 Lecture 41

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: 0
+            count: props.count
         };
     }
     handleAddOne() {
@@ -41,35 +41,8 @@ class Counter extends React.Component {
   }
 }
 
+Counter.defaultProps = {
+    count: 0
+}
 
 ReactDOM.render(<Counter />, document.getElementById('app'))
-
-// let count = 0;
-
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-// };
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// };
-// const reset = () => {
-//     count = 0;
-//     renderCounterApp();
-// };
-
-
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={addOne}>+1</button>
-//             <button onClick={reset}>reset</button>
-//         </div>
-//     );
-//     ReactDOM.render(templateTwo, appRoot);
-// }
-
-// renderCounterApp();
